a07a793e65e17e72f25931b6fdbfaad98e0ad11f
wizaard07 <shekhatniral18@gmail.com>
2024-09-13 11:00:09 +0530

    Authentication

---
+++ backend/web_backend/config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: 'digimanuf'
+    });
+
+    console.log(`✅ MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
+
+//mongodb+srv://root:<password>@cluster0.qlxbzre.mongodb.net/
