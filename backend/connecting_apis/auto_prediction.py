69cc67a57e4620e7efb830b0989dad9d24d85e8e
Neel Shah <neeldevenshah@gmail.com>
2024-10-02 18:30:22 +0530

    Updated some conflicting ports

--- backend/connecting_apis/auto_prediction.py
+++ backend/connecting_apis/auto_prediction.py
@@ -206,4 +206,4 @@ async def start_background_tasks():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8003)
